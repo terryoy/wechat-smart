26a207a00b360b20f3ec1869f839a183bcf7b655
terryoy <terry.ouyang@gmail.com>
2015-06-25 13:13:04 +0000

    add wechat cgi api

--- app/wechatsmart/models.py
+++ app/wechatsmart/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
+from .wechat_api import WechatCgiApi
 
 # Create your models here.
+class WechatConfig(models.Model):
+
+    # Configuration Keys
+    KEY_MENU_JSON = "wechat_menu_json"
+    KEY_ACCESS_TOKEN = "wechat_access_token"
+
+
+    key = models.CharField(max_length=128, choices=(
+        (KEY_MENU_JSON, KEY_MENU_JSON),
+        (KEY_ACCESS_TOKEN, KEY_ACCESS_TOKEN),
+        ))
+    value = models.CharField(max_length=1024, null=True, blank=True)
+    expire_date = models.DateTimeField(null=True, blank=True)
+    
+    def is_expired(self):
+        return self.expire_date and self.expire_date < timezone.now()
+
+    @classmethod
+    def refresh_access_token(cls):
+        api = WechatCgiApi(app_id=settings.WECHAT_APP_ID, app_secret=settings.WECHAT_APP_SECRET)
+        result_api = api.get_access_token()
+        if result_api and result_api[u"access_token"]:
+            token = result_api[u"access_token"]
+            expire_date = timezone.now() + timezone.timedelta(seconds=result_api[u"expires_in"])
+
+            token_config, created = WechatConfig.objects.get_or_create(key=WechatConfig.KEY_ACCESS_TOKEN)
+            token_config.value = token
+            token_config.expire_date = expire_date
+            token_config.save()
+
+            return True, result_api
+        else:
+            return False, result_api
+
---
+++ app/wechatsmart/wechat_api.py
@@ -0,0 +1,84 @@
+import logging, json
+import urllib2
+import requests
+
+log = logging.getLogger(__name__)
+
+class WechatCgiApi(object):
+    API_URL = "https://api.weixin.qq.com/cgi-bin/{0}?{1}"
+
+    def __init__(self, app_id="", app_secret=""):
+        self.app_id = app_id
+        self.app_secret = app_secret
+
+    def _get_entry(self, endpoint, param_dict):
+        param_str = ""
+        for k, v in param_dict.iteritems():
+            param_str += "{0}={1}&".format(k, v)
+        return self.API_URL.format(endpoint, param_str)
+
+    def _send_get_request(self, url):
+        # send request with urllib2
+        resp = urllib2.urlopen(url)
+        content = resp.read()
+        # convert json data to dictionary
+        try:
+            return json.loads(content)
+        except Exception as e:
+            log.error("Get request error: {0} content:{1}".format(e, content))
+            return None
+        
+    def _send_post_request(self, url, body):
+        # send request
+        #resp = urllib2.urlopen(url, data=body)
+        #content = resp.read()
+        r = requests.post(url, data=body)
+        content = r.text
+
+        # convert json data to dictionary
+        try:
+            return json.loads(content)
+        except Exception as e:
+            log.error("Post request error: {0} content:{1}".format(e, content))
+            return None
+
+    def _send_post_file_request(self, url, params, files):
+        r = requests.post(url, data=params, files=files)
+        content = r.text
+
+        # convert json data to dictionary
+        try:
+            return json.loads(content)
+        except Exception as e:
+            log.error("Post request error: {0} content:{1}".format(e, content))
+            return None
+
+
+    def get_access_token(self, grant_type="client_credential"):
+        url = self._get_entry("token", {"appid": self.app_id, "secret":self.app_secret, "grant_type":grant_type})
+        log.debug("Get access token: {0}".format(url)) # only debug can print this url, because app secret is sensitive
+
+        return self._send_get_request(url)
+
+    def get_wechat_ip(self, access_token):
+        url = self._get_entry("getcallbackip", {"access_token": access_token})
+        log.info("Get wechat ip: {0}".format(url))
+
+        return self._send_get_request(url)
+
+    def create_custom_menu(self, access_token, menu_dict={}, content=None):
+        url = self._get_entry("menu/create", {"access_token":access_token})
+        if content:
+            body = content.encode("gb2312")
+        else:
+            body = json.dumps(menu_dict)
+        log.info(u"Create custom menu: {0}\nPost: {1}".format(url, body))
+
+        return self._send_post_request(url, body)
+
+    def create_temp_media(self, access_token, media_type, media={}):
+        url = self._get_entry("media/upload", {"access_token":access_token, "type":media_type})
+        return self._send_post_file_request(url, params={}, files=media)
+
+        
+        
